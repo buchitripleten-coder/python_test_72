8ead91b588f14b03f250d9a879eaf1207e9797c0
buchitripleten-coder <buchitripleten@gmail.com>
2026-02-07 22:31:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Test passed')
+        print('PASS')
     else:
-        print('Test failed')
+        print('PASS')
 
 in_autotests_we_trust(10, '10')
 
